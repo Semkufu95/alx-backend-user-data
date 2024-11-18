26bb399cbe44e160a3e1d25129b12111d6b5777b
semkufu95 <semkufu95@gmail.com>
2024-11-18 04:45:49 +0300

    updated  to format and filter logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ A function to return the log message obfuscated
 '''
 
 import re
+import logging
 from typing import List
 
 
@@ -13,3 +14,27 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = f"({'|'.join(map(re.escape, fields))})=.+?{separator}"
     return re.sub(pattern,
                   lambda m: f"{m.group(1)}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        '''
+        initialize the formatter with fields to redact
+        '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields: List[str] = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        Format a log record redacting sensitive fields
+        '''
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
