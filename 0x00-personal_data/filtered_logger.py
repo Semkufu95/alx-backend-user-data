e7187bb00e69f5942deb90ba849d385f7fed91c2
semkufu95 <semkufu95@gmail.com>
2024-11-18 04:16:42 +0300

    Regexing correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,12 @@ A function to return the log message obfuscated
 '''
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    ''' return obfuscated log message '''
     pattern = f"({'|'.join(map(re.escape, fields))})=.+?{separator}"
     return re.sub(pattern,
                   lambda m: f"{m.group(1)}={redaction}{separator}", message)
